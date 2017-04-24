8cfa27ec12ba20ebf682154d27ae867ab7f98b3d
Rana Ali <ranaali1810@gmail.com>
2017-04-24 21:33:41 +0500

    Test Post /todos routes

---
+++ server/tests/server.test.js
@@ -0,0 +1,47 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+
+//remove all todos fetch from database
+beforeEach((done) => {
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+
+  it('should create a new todo', (done) => {
+    let text = 'Test todo text';
+
+    request(app).post('/todos').send({text}).expect(200).expect((res) => {
+
+      expect(res.body.text).toBe(text);
+
+    }).end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      Todo.find().then((todos) => {
+        // expect(todos.length).toBe(1);
+        expect(todos[0].text).toBe(text);
+        done();
+      }).catch((err) => done(err));
+    });
+  });
+
+  it('should not create with invalid body data', (done) => {
+
+    request(app).post('/todos').send({}).expect(400).end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+
+      Todo.find().then((todos) => {
+        expect(todos.length).toBe(0);
+        done();
+      }).catch((err) => done(e));
+
+    });
+  });
+});
