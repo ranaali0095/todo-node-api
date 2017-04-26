a8939a01f5e747432d5aa86fcfdf0f03ffa6daf3
Rana Ali <ranaali1810@gmail.com>
2017-04-27 00:18:34 +0500

    Add POST /user route and token method

--- server/models/users.js
+++ server/models/users.js
@@ -1,12 +1,57 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-let User = mongoose.model('Users', {
+let UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
     minlength: 3,
+    unique: true,
+    validate: [
+      {
+        validator: value => validator.isEmail(value),
+        msg: 'Invalid email.',
+      }],
   },
-});
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true,
+      },
+      token: {
+        type: String,
+        required: true,
+      },
+    }],
+})
 
-module.exports = {User};
+UserSchema.methods.toJSON = function () {
+  let user = this
+  let userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email'])
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  let user = this
+  let access = 'auth'
+  let token = jwt.sign({_id: user._id.toHexString(), access}, 'somerandomtext')
+
+  user.tokens.push({access, token})
+
+  return user.save().then(() => {
+    return token
+  })
+}
+
+let User = mongoose.model('Users', UserSchema)
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -106,6 +106,21 @@ app.patch('/todos/:id', (req, res) => {
 
 })
 
+app.post('/users', (req, res) => {
+
+  let body = _.pick(req.body,
+    ['email', 'password'])
+  let user = new User(body)
+  user.save().then(() => {
+    return user.generateAuthToken()  //call an  instance method
+  }).then((token) => {
+    console.log(token);
+    res.header('x-auth', token).send(user)
+  }).catch((err) => {
+    res.status(400).send(err)
+  })
+})
+
 //start server
 app.listen(port, () => {
   console.log('Started on port ', port)
